2207684421c19aade71821a670ccc0483ed5e47d
malindu20 <59385501+malindu20@users.noreply.github.com>
2024-02-25 11:14:37 -0500

    Update midtermapp.py

--- midtermapp.py
+++ midtermapp.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 # Define a route to display company name, developer's name, and student ID
 @app.route('/')
 def index():
-    company_name = "Wild Rydes"
-    developer_name = "Malindu Mahara"
-    student_id = "100944755"
-    return f"Company Name: {company_name}<br>Developer: {developer_name}<br>Student ID: {student_id}"
+    company_name = "<span style='font-weight: bold; color: #007bff;'>Wild Rydes</span>"
+    developer_name = "Developer: Mahara Mahara"
+    student_id = "Student ID: 100944755"
+    return f"<h1>{company_name}</h1><br>{developer_name}<br>{student_id}"
  
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
